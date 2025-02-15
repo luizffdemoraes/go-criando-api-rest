754c9372c53bc0af9c3698e0d341c363d728c35e
luizffdemoraes <lffm1994@gmail.com>
2025-02-15 18:31:47 -0300

    Variáveis e convenções

--- main.go
+++ main.go
@@ -3,5 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World!")
+
+	/***
+	var Declaration
+	var nomePizzaria string
+	***/
+	// := Short Assignment Statement
+	nomePizzaria := "Pizzaria Go"
+	instagram, telefone := "@pizzaria_go", 11951
+	fmt.Println(nomePizzaria, instagram, telefone)
 }
