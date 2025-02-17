974d825b73c43e5af4f703413f3c650a97dff055
luizffdemoraes <lffm1994@gmail.com>
2025-02-16 23:12:19 -0300

    Integrando structs

--- internal/handler/review.go
+++ internal/handler/review.go
@@ -1,7 +1,37 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+	"pizzaria/internal/data"
+	"pizzaria/internal/models"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
 
 func PostReviewsByIDPizza(c *gin.Context) {
-	
+	pizzaID, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+		return
+	}
+
+	var newReview models.Review
+	if err := c.ShouldBindJSON(&newReview); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+		return
+	}
+
+	for i, p := range data.Pizzas {
+		if p.ID == pizzaID {
+			p.Reviews = append(p.Reviews, newReview)
+			data.Pizzas[i] = p
+			data.SavePizzas()
+			c.JSON(http.StatusCreated, gin.H{"message": "Review created"})
+			return
+		}
+
+		c.JSON(http.StatusNotFound, gin.H{"message": "Pizza not found"})
+	}
+
 }
