1cf53d6b01011caac4eba932d0ef9de9c47ee743
luizffdemoraes <lffm1994@gmail.com>
2025-02-16 23:04:16 -0300

    Composição

--- cmd/main.go
+++ cmd/main.go
@@ -17,5 +17,6 @@ func main() {
 	router.GET("/pizzas/:id", handler.GetPizzasByID)
 	router.DELETE("/pizzas/:id", handler.DeletePizzasByID)
 	router.PUT("/pizzas/:id", handler.UpdatePizzasByID)
+	router.POST("/pizzas/:id/reviews", handler.PostReviewsByIDPizza)
 	router.Run() // listen and serve on 0.0.0.0:8080
 }
